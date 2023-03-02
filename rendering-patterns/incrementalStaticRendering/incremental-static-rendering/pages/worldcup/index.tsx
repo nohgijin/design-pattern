cb65eccdf89986d7796ffd13f17bc75cd4c8ee94
nohgijin <ynda1111@naver.com>
2023-03-03 01:35:10 +0900

    incremental static rendering example

--- rendering-patterns/incrementalStaticRendering/incremental-static-rendering/pages/worldcup/index.tsx
+++ rendering-patterns/incrementalStaticRendering/incremental-static-rendering/pages/worldcup/index.tsx
@@ -3,14 +3,12 @@ export default function WorldCup( {time}: {time:any} ){
 }
 
 export async function getStaticProps() {
-    // API Call to get current time
     const res = await fetch('https://worldtimeapi.org/api/ip');
     const time = await res.json();
     return {
         props: {
             time
         },
-        // Regenerate the page after 5 seconds
         revalidate: 5
     };
 }
